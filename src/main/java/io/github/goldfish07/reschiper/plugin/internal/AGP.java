92ead311a4765a384f772df3ac7357cf25a45dab
Ayush Bisht <ayushbisht5663@gmail.com>
2026-03-11 19:32:01 +0530

    [internal][agp] replace internal component identifier lookup

--- src/main/java/io/github/goldfish07/reschiper/plugin/internal/AGP.java
+++ src/main/java/io/github/goldfish07/reschiper/plugin/internal/AGP.java
@@ -2,19 +2,33 @@ package io.github.goldfish07.reschiper.plugin.internal;
 
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.ModuleVersionIdentifier;
+import org.gradle.api.artifacts.ResolvedArtifact;
+import org.gradle.api.artifacts.ResolvedModuleVersion;
 import org.gradle.api.initialization.dsl.ScriptHandler;
-import org.gradle.internal.component.external.model.DefaultModuleComponentIdentifier;
 import org.jetbrains.annotations.NotNull;
 
 public class AGP {
     public static @NotNull String getAGPVersion(@NotNull Project project) {
         String agpVersion = null;
-        for (org.gradle.api.artifacts.ResolvedArtifact artifact : project.getRootProject().getBuildscript().getConfigurations().getByName(ScriptHandler.CLASSPATH_CONFIGURATION)
-                .getResolvedConfiguration().getResolvedArtifacts()) {
-            DefaultModuleComponentIdentifier identifier = (DefaultModuleComponentIdentifier) artifact.getId().getComponentIdentifier();
-            if ("com.android.tools.build".equals(identifier.getGroup()) || 432891823 == identifier.getGroup().hashCode())
-                if ("gradle".equals(identifier.getModule()))
-                    agpVersion = identifier.getVersion();
+        for (ResolvedArtifact artifact : project.getRootProject().getBuildscript()
+                .getConfigurations()
+                .getByName(ScriptHandler.CLASSPATH_CONFIGURATION)
+                .getResolvedConfiguration()
+                .getResolvedArtifacts()) {
+            ResolvedModuleVersion moduleVersion = artifact.getModuleVersion();
+            if (moduleVersion == null)
+                continue;
+
+            ModuleVersionIdentifier identifier = moduleVersion.getId();
+            if (identifier == null)
+                continue;
+
+            if ("com.android.tools.build".equals(identifier.getGroup())
+                    && "gradle".equals(identifier.getName())) {
+                agpVersion = identifier.getVersion();
+                break;
+            }
         }
         if (agpVersion == null)
             throw new GradleException("Failed to get AGP version");
